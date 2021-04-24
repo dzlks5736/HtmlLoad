fb99f1aa7083fdb56482dee5e515cddd8e029daf
Siu <dzlks6375@gmail.com>
2021-04-25 06:37:17 +0900

    Initial Commit

---
+++ HtmlLoad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HtmlLoad
+//
+//  Created by SIU on 2021/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
